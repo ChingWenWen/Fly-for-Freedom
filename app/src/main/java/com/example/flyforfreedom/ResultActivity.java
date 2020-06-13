fc96fc7bc747fb2744ab261bec731b208abb9eff
ChingWenWen <box789987@gmail.com>
2020-06-13 15:38:07 +0800

    結果頁面草稿

--- app/src/main/java/com/example/flyforfreedom/ResultActivity.java
+++ app/src/main/java/com/example/flyforfreedom/ResultActivity.java
@@ -3,6 +3,10 @@ package com.example.flyforfreedom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
 
 public class ResultActivity extends AppCompatActivity {
 
