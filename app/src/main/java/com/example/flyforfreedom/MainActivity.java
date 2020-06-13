b7813a09716d247e3dd164b3f2bfaeb8b89dbc74
ChingWenWen <box789987@gmail.com>
2020-06-13 15:51:09 +0800

    切換頁面

--- app/src/main/java/com/example/flyforfreedom/MainActivity.java
+++ app/src/main/java/com/example/flyforfreedom/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.flyforfreedom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button go;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        go = findViewById(R.id.go);
+    }
+
+    public void onClick(View v){
+        Intent intent = new Intent(this, ChooseAirplaneActivity.class);
+        startActivity(intent);
     }
 }
